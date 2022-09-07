ea09ea2f14ea2d1048849d2413e1680398819b1e
Vladuslav Dragushin <maxhc@inbox.ru>
2022-09-07 11:21:20 +0300

    added payload for GET

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import requests
 
 
-# url = 'https://wttr.in/London'
-# url = 'https://wttr.in/Череповец'
-url = 'https://wttr.in/svo'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+URL_TEMPLATE = 'https://wttr.in/{}'
+LOCATIONS = ('London', 'Череповец', 'svo')
+
+
+def main():
+    for location in LOCATIONS:
+        payload = {
+            'lang': 'ru',
+            'M': '',
+            'u': '',
+            'n': '',
+            'T': '',
+            'q': '',
+
+        }
+        url = URL_TEMPLATE.format(location)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+
+if __name__ == '__main__':
+    main()
