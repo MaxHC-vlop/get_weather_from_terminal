59387092316d0d28fedf87722182fef1cb0f7ee9
Vladuslav Dragushin <maxhc@inbox.ru>
2022-09-07 11:04:07 +0300

    view weather London, Moscow, SVO

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 
 
-url = 'https://wttr.in/san%20francisco?nTqu&lang=en'
+# url = 'https://wttr.in/London'
+# url = 'https://wttr.in/Череповец'
+url = 'https://wttr.in/svo'
 response = requests.get(url)
 response.raise_for_status()
 print(response.text)
